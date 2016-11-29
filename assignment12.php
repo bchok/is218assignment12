74defa9a97d5dade90900ec5a5e6e9504e443533
bchok <bchok91@gmail.com>
2016-11-28 19:53:41 -0500

    added post function to the class

--- assignment12.php
+++ assignment12.php
@@ -15,11 +15,39 @@ class curlGETPOST{
 		return $output;
 
 	}
+	
+	function httpPost($url, $param){
+		
+		$postData = '';
+		
+		foreach($param as $key => $value){
+			$postData .= $key . '=' . $value . '&';
+		}
+		
+		$postData = rtrim($postData, '&');
+		
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_POST, count($postData));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+		$output = curl_exec($ch);
+
+		curl_close($ch);
+		return $output;
+	}
 }
 
 $curlTest = new curlGETPOST();
 print_r($curlTest);
 echo $curlTest->httpGet("http://njit.edu");
+
+$param = array("name" => "Brian", "age" => "26");
+
+echo $curlTest->httpPost("http://web.njit.edu/~bjc36/is218assignments/assignment12/assignment12.php", $param);
 
 
 
