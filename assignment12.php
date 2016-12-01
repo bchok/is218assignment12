3d89ca309c6e6f2dad20a683d923c94c7897a201
bchok <bchok91@gmail.com>
2016-12-01 17:55:32 -0500

    added error and exception handling to my curl class.  Also added some formatting so the web output looks neater

--- assignment12.php
+++ assignment12.php
@@ -1,53 +1,73 @@
 <?php
-	
+
 class curlGETPOST{
 
 	function httpGet($url){
-	
-		$ch = curl_init();
-		
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		
-		$output = curl_exec($ch);
-		
-		curl_close($ch);
-		return $output;
 
-	}
-	
-	function httpPost($url, $param){
-		
-		$postData = '';
-		
-		foreach($param as $key => $value){
-			$postData .= $key . '=' . $value . '&';
-		}
-		
-		$postData = rtrim($postData, '&');
-		
-		$ch = curl_init();
-		
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_POST, count($postData));
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+		try{
+			$ch = curl_init();
 
-		$output = curl_exec($ch);
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		curl_close($ch);
-		return $output;
+			$output = curl_exec($ch);
+
+			if(curl_errno($ch)){
+				throw new Exception(curl_error($ch));
+			}
+
+			curl_close($ch);
+			return $output;
+
+	}catch(Exception $e){
+		echo $e->getMessage();
 	}
 }
 
+	function httpPost($url, $param){
+
+		try{
+			$postData = '';
+
+			foreach($param as $key => $value){
+				$postData .= $key . '=' . $value . '&';
+			}
+
+			$postData = rtrim($postData, '&');
+
+			$ch = curl_init();
+
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_POST, count($postData));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+			$output = curl_exec($ch);
+
+			if(curl_errno($ch)){
+				throw new Exception(curl_error($ch));
+			}
+
+			curl_close($ch);
+			return $output;
+
+	}catch(Exception $e){
+		echo $e->getMessage();
+	}
+}
+}
+
 $curlTest = new curlGETPOST();
+echo 'Proof that a curl object was created<br>';
 print_r($curlTest);
+echo'<br><br> Result from get a curl get request on njit.edu<br>';
 echo $curlTest->httpGet("http://njit.edu");
 
 $param = array("name" => "Brian", "age" => "26");
 
-echo $curlTest->httpPost("http://web.njit.edu/~bjc36/is218assignments/assignment12/assignment12.php", $param);
+echo '<br><br> Result of a curl post request on google.com with a given array<br>';
+echo $curlTest->httpPost("http://www.google.com", $param);
 
 
 
